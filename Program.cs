663029efbe46155764590174efd62f19f226bce6
Arsalan <ars89632535696@gmail.com>
2018-09-16 01:12:59 +0700

    Japan

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("это в GitHub");
+            var s = DateTime.Now;
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
